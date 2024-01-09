4167c312d9df4a391c4378991f8e7218bfa73536
romicons <romina.constantinoff@gmail.com>
2024-01-08 22:41:59 -0300

    Added styles to the meme-container. Changed the function updateImageDisplay to overwrite the source of an existing img and not create a new img element.

--- index.js
+++ index.js
@@ -11,64 +11,73 @@ let darkMode = localStorage.getItem('darkMode');
 const darkModeToggle = document.querySelector('#btn-darkmode');
 
 const enableDarkMode = () => {
-    document.body.classList.add('darkmode');
-    localStorage.setItem('darkMode', 'enabled');
+  document.body.classList.add('darkmode');
+  localStorage.setItem('darkMode', true);
 }
 
+
 const disableDarkMode = () => {
-    document.body.classList.remove('darkmode');
-    localStorage.setItem('darkMode', null);
+  document.body.classList.remove('darkmode');
+  localStorage.setItem('darkMode', false);
 }
 
 darkModeToggle.addEventListener('click', () => {
-    darkMode = localStorage.getItem('darkMode'); 
-    if (darkMode !== 'enabled') {
-        enableDarkMode();
-    } else {  
-        disableDarkMode(); 
-  }
+  darkMode = JSON.parse(localStorage.getItem('darkMode'));
+
+  if (!darkMode) {
+      enableDarkMode();
+  } else {  
+      disableDarkMode(); 
+}
 });
 
 // FUNCTION TO UPLOAD THE IMAGE
 
 const fileTypes = [
-    "image/apng",
-    "image/jpeg",
-    "image/pjpeg",
-    "image/png",
-  ];
-  
+  "image/apng",
+  "image/jpeg",
+  "image/pjpeg",
+  "image/png",
+];
+
 const validFileType = (file) => {
-    return fileTypes.includes(file.type);
+  return fileTypes.includes(file.type);
 }
 
 uploadImg.style.opacity = 0;
 
 const updateImageDisplay = () => {
-    while (errorTxt.firstChild) {
+  while (errorTxt.firstChild) {
       errorTxt.removeChild(errorTxt.firstChild);
-    }
-  
-    const currentFile = fileImg.files[0];
-  
-    if (!currentFile) {
+  }
+
+  const currentFile = fileImg.files[0];
+
+  if (!currentFile) {
       const noImg = document.createElement("p");
       noImg.textContent = "No has cargado ninguna imagen.";
+      noImg.style.color = "red";
       errorTxt.appendChild(noImg);
-    } else {
+  } else {
       if (validFileType(currentFile)) {
-        // Crear una vista previa de la imagen
-        const image = document.createElement("img");
-        image.src = URL.createObjectURL(currentFile);
-        image.alt = image.title = currentFile.name;
-        imgMemeContainer.appendChild(image);
+          const existingImage = document.getElementById("img-container");
+          if (existingImage) {
+              existingImage.src = URL.createObjectURL(currentFile);
+          } else {
+              const txtInvalidFile = document.createElement("p");
+              txtInvalidFile.textContent = "No se encontró ninguna imagen. Por favor, intente nuevamente.";
+              txtInvalidFile.style.color = "red";
+              errorTxt.appendChild(txtInvalidFile);
+          }
       } else {
-        const txtInvalidFile = document.createElement("p");
-        txtInvalidFile.textContent = `Nombre del archivo ${currentFile.name}: No es un archivo válido. Por favor, elige un archivo válido.`;
-        errorTxt.appendChild(txtInvalidFile);
+          const txtInvalidFile = document.createElement("p");
+          txtInvalidFile.textContent = `${currentFile.name}: No es un archivo válido. Por favor, elige un archivo válido.`;
+          txtInvalidFile.style.color = "red";
+          errorTxt.appendChild(txtInvalidFile);
       }
-    }
-  };
+  }
+};
+
 
 // EVENT LISTENER
 
